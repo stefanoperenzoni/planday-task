1e9ba22336da9d735d6eff29ded9ece46a418d92
stefano.perenzoni <stefano.perenzoni@spaziodati.eu>
2024-10-02 21:12:30 +0200

    Revert changes on trial_period

--- models/intermediate/conversion/trial_period.sql
+++ models/intermediate/conversion/trial_period.sql
@@ -1,41 +1,29 @@
 {{
     config(
-        unique_key='organization_id'
+        materialized='incremental',
+        unique_key = 'organization_id',
+        cluster_by = ["organization_id"],
+        incremental_strategy = 'merge',
+        merge_update_columns = ['max_timestamp', 'trial_timedelta']
     )
 }}
 
-WITH new_data AS (
-    SELECT
-        "ORGANIZATION_ID" AS organization_id,
-        MIN("TIMESTAMP") AS min_timestamp,
-        MAX("TIMESTAMP") AS max_timestamp
+{% if is_incremental() %}
+WITH organzation_changed as (
+    SELECT DISTINCT "ORGANIZATION_ID" as organization_id
     FROM {{ source('planday', 'interactions') }}
-    {% if is_incremental() %}
-        -- Only process new rows in the incremental run
-        WHERE "TIMESTAMP" > (SELECT COALESCE(MAX(max_timestamp), '1970-01-01') FROM {{ this }})
-    {% endif %}
-    GROUP BY "ORGANIZATION_ID"
-),
-
-existing_data AS (
-    {% if is_incremental() %}
-    SELECT *
-    FROM {{ this }}
-    {% else %}
-    SELECT
-        NULL AS organization_id,
-        TIMESTAMP '0001-01-01 00:00:01.000' AS min_timestamp,
-        TIMESTAMP '0001-01-01 00:00:01.000' AS max_timestamp,
-        NULL as trial_timedelta
-    WHERE 1 = 0
-    {% endif %}
+    WHERE "TIMESTAMP" > (SELECT MAX("max_timestamp") from {{this}})
 )
 
+{% endif %}
+
 SELECT
-    ed.organization_id AS organization_id,
-    LEAST(nd.min_timestamp, ed.min_timestamp) AS min_timestamp,
-    GREATEST(nd.max_timestamp, ed.max_timestamp) AS max_timestamp,
-    (GREATEST(nd.max_timestamp, ed.max_timestamp) - LEAST(nd.min_timestamp, ed.min_timestamp)) AS trial_timedelta
-FROM existing_data ed
-FULL OUTER JOIN new_data nd
-ON ed.organization_id = nd.organization_id
+"ORGANIZATION_ID" as organization_id,
+MIN("TIMESTAMP") as min_timestamp,
+MAX("TIMESTAMP") as max_timestamp,
+MAX("TIMESTAMP") - MIN("TIMESTAMP") as trial_timedelta
+FROM {{ source('planday', 'interactions') }}
+{% if is_incremental() %}
+  WHERE "ORGANIZATION_ID" in (select organization_id from organzation_changed)
+{% endif %}
+GROUP BY "ORGANIZATION_ID"
