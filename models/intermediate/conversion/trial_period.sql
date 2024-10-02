a0821aaaa19760592cc36d38f7438aad43e5212b
stefano.perenzoni <stefano.perenzoni@spaziodati.eu>
2024-10-02 19:31:56 +0200

    Improve trial_period

--- models/intermediate/conversion/trial_period.sql
+++ models/intermediate/conversion/trial_period.sql
@@ -3,7 +3,8 @@
         materialized='incremental',
         unique_key = 'organization_id',
         cluster_by = ["organization_id"],
-        incremental_strategy = 'merge'
+        incremental_strategy = 'merge',
+        merge_update_columns = ['max_timestamp']
     )
 }}
 
@@ -17,12 +18,18 @@ WITH organzation_changed as (
 {% endif %}
 
 SELECT
+    organization_id,
+    min_timestamp,
+    max_timestamp,
+    (max_timestamp - min_timestamp) as trial_timedelta
+FROM (
+    SELECT
     "ORGANIZATION_ID" as organization_id,
     MIN("TIMESTAMP") as min_timestamp,
-    MAX("TIMESTAMP") as max_timestamp,
-    (MAX("TIMESTAMP") - MIN("TIMESTAMP") ) as trial_timedelta
+    MAX("TIMESTAMP") as max_timestamp
     FROM {{ source('planday', 'interactions') }}
     {% if is_incremental() %}
       WHERE "ORGANIZATION_ID" in (select organization_id from organzation_changed)
     {% endif %}
     GROUP BY "ORGANIZATION_ID"
+) q
